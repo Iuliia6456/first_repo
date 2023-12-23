5f825dbf8599e7ec2a14386c1fa3987830865e91
IZhukova <iuliia.zhukova@avanquest.com>
2023-12-23 23:49:49 +0100

    uuid, try except, logging added

--- HW2_Task1.py
+++ HW2_Task1.py
@@ -1,36 +1,40 @@
 import queue
-import random
+import uuid
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(message)s')
 
 request_queue = queue.Queue()
 
 def generate_request():
-    user_input = input("\nPlease select a subject of your request or press exit: ").lower()
+    user_input = input("Please select a subject of your request or press 'exit' to end: ").lower()
     if user_input == "exit":
         return user_input
-    new_request = {"id": random.randint(1, 10), "subject": user_input}
+
+    new_request = {"id": str(uuid.uuid4()), "subject": user_input}
     request_queue.put(new_request)
-    print(f"\n The request {new_request} is registered")
-    print("\n Queue: ", list(request_queue.queue))
-   
+    logging.info(f"\nThe request {new_request} is registered\n")
+    logging.info("Queue: %s\n", list(request_queue.queue))
+
 def process_request():
     if not request_queue.empty():
         del_request = request_queue.get()
-        print(f"\n The request {del_request} is proceeded")
-        print("\n Queue: ", list(request_queue.queue))
+        logging.info(f"The request {del_request} is executed\n")
+        logging.info("Updated queue after executed request: %s\n", list(request_queue.queue))
     else:
-        print("The queue is empty")
+        logging.info("The queue is empty")
 
 def main():
+    # adding a few requests to the queue before running our main loop to demonstrate the queue logic
+    request_queue.put({'id': str(uuid.uuid4()), 'subject': 'My first request'})
+    request_queue.put({'id': str(uuid.uuid4()), 'subject': 'My second request'})
 
-    # adding a few requests to the queue before running our main loop to demostrate the queue logic
-    request_queue.put({'id': 0, 'subject': 'My first request'})
-    request_queue.put({'id': 1, 'subject': 'My second request'})
-
-    while True:
-        user_input = generate_request()
-        if user_input == "exit":
-            break
-        process_request()
+    try:
+        while True:
+            generate_request()
+            process_request()
+    except KeyboardInterrupt:
+        logging.info("\nProgram finished\n")
 
 if __name__ == "__main__":
     main()
